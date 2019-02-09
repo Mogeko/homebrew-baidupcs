aa6b958feec44824f93ff66faa8cf077f7399e40
Mogeko <zhengjunyi@live.com>
2019-02-10 02:22:49 +0800

    fix

--- Formula/baidupcs-go.rb
+++ Formula/baidupcs-go.rb
@@ -1,11 +1,24 @@
 class BaidupcsGo < Formula
-    desc "仿 Linux shell 文件处理命令的百度网盘命令行客户端"
-    homepage "https://github.com/iikira/BaiduPCS-Go"
-    url "https://github.com/iikira/BaiduPCS-Go/releases/download/v3.5.6/BaiduPCS-Go-v3.5.6-darwin-osx-amd64.zip"
-    version "3.5.6"
-    sha256 "db087e64d606639ca3e05a4b482174a06d3c0eabd6cbdbcc317374fc40742a96"
+  desc "仿 Linux shell 文件处理命令的百度网盘命令行客户端"
+  homepage "https://github.com/iikira/BaiduPCS-Go"
+  version "3.5.6"
+  url "https://github.com/iikira/BaiduPCS-Go/archive/v#{version}.tar.gz"
+  sha256 "0b2abd18c5101d5255faa31c713cbebad3804af07c1a008a5b9b799060beb12a"
 
-    def install
-        bin.install "BaiduPCS-Go"
-    end
+  depends_on "go" => :build
+
+  def install
+    ENV["GOPATH"] = buildpath
+    (buildpath/"src/github.com/iikira").mkpath
+    ln_s buildpath, buildpath/"src/github.com/iikira/BaiduPCS-Go"
+    system "go", "get", "github.com/iikira/BaiduPCS-Go"
+
+    bin.install "bin/BaiduPCS-Go"
+
+    bin.install_symlink "BaiduPCS-Go" => "baidupcs"
+  end
+
+  test do
+    system "#{bin}/BaiduPCS-Go", "--version"
+  end
 end
